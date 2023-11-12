62f35287938b71a082d6569696bf2aea84be5faf
David <104325852+Cdaprod@users.noreply.github.com>
2023-11-11 22:42:42 -0500

    Add populate_fake_data function to create fake credentials for testing
    - Add populate_fake_data function to create fake credentials for testing
    - Add test_read_credential_with_fake_data function to test reading a
    cre
    dential with fake data
    - Add create_test_credential fixture to create a test credential
    - Rename test_read_credential function to
    test_read_credential_with_sing
    le_entry

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,6 +9,28 @@ from models import CredentialDB, CredentialType
 client = TestClient(app)
 
 @pytest.fixture(scope="function")
+def populate_fake_data(db_session):
+    fake_credentials = [
+        CredentialDB(name="test1", value="value1", type=CredentialType.API_KEY),
+        CredentialDB(name="test2", value="value2", type=CredentialType.SSH_KEY),
+        # Add as many fake credentials as needed
+    ]
+
+    for credential in fake_credentials:
+        db_session.add(credential)
+    db_session.commit()
+
+    yield
+
+    for credential in fake_credentials:
+        db_session.delete(credential)
+    db_session.commit()
+
+def test_read_credential_with_fake_data(populate_fake_data, db_session):
+    response = client.get("/credentials/test1")
+    assert response.status_code == 200
+
+@pytest.fixture(scope="function")
 def create_test_credential(db_session):
     credential = CredentialDB(name="test", value="test_value", type=CredentialType.API_KEY)
     db_session.add(credential)
@@ -17,6 +39,6 @@ def create_test_credential(db_session):
     db_session.delete(credential)
     db_session.commit()
 
-def test_read_credential(create_test_credential, db_session):
+def test_read_credential_with_single_entry(create_test_credential, db_session):
     response = client.get("/credentials/test")
     assert response.status_code == 200
