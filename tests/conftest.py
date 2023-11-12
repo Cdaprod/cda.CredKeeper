7619f4b56d4ec8ccc0f3c347ad63bf20537f49e9
David <104325852+Cdaprod@users.noreply.github.com>
2023-11-11 22:29:39 -0500

    Add conftest.py and test_api.py to tests directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
+from __init__ import Base
 
 @pytest.fixture(scope="function")
 def db_session():
