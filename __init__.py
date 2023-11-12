6d7c06d03b375a23e6cae0c0f7da908aa86c6a0b
David <104325852+Cdaprod@users.noreply.github.com>
2023-11-11 21:56:12 -0500

    Refactor engine to db_engine in main.py

--- __init__.py
+++ __init__.py
@@ -7,6 +7,6 @@ DATABASE_URL = get_environment_variable("DATABASE_URL", "sqlite:///./test.db")
 
 db_engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 Base = declarative_base()
