9d2dde3c445853a350a195956e96ec56204bc634
David <104325852+Cdaprod@users.noreply.github.com>
2023-11-11 23:02:22 -0500

    Add populate_fake_data function to create fake credentials for testing
    - Add populate_fake_data function to create fake credentials for testing
    - Add test_read_credential_with_fake_data function to test reading a
    cre
    dential with fake data
    - Add create_test_credential fixture to create a test credential
    - Rename test_read_credential function to
    test_read_credential_with_sing
    le_entry

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,35 +10,13 @@ client = TestClient(app)
 
 @pytest.fixture(scope="function")
 def populate_fake_data(db_session):
-    fake_credentials = [
-        CredentialDB(name="test1", value="value1", type=CredentialType.API_KEY),
-        CredentialDB(name="test2", value="value2", type=CredentialType.SSH_KEY),
-        # Add as many fake credentials as needed
-    ]
-
-    for credential in fake_credentials:
-        db_session.add(credential)
-    db_session.commit()
-
+    test_credential = CredentialDB(name="test1", value="value1", type=CredentialType.API_KEY)
+    db_session.add(test_credential)
+    db_session.commit()  # Commit the session to save the data
     yield
-
-    for credential in fake_credentials:
-        db_session.delete(credential)
-    db_session.commit()
+    db_session.delete(test_credential)
+    db_session.commit()  # Clean up after the test
 
 def test_read_credential_with_fake_data(populate_fake_data, db_session):
     response = client.get("/credentials/test1")
-    assert response.status_code == 200
-
-@pytest.fixture(scope="function")
-def create_test_credential(db_session):
-    credential = CredentialDB(name="test", value="test_value", type=CredentialType.API_KEY)
-    db_session.add(credential)
-    db_session.commit()
-    yield
-    db_session.delete(credential)
-    db_session.commit()
-
-def test_read_credential_with_single_entry(create_test_credential, db_session):
-    response = client.get("/credentials/test")
     assert response.status_code == 200
