07047626382515da3918c96a18574f7aa7e6117e
David <104325852+Cdaprod@users.noreply.github.com>
2023-11-11 23:05:46 -0500

    Add populate_fake_data function to create fake credentials for testing
    - Add populate_fake_data function to create fake credentials for testing
    - Add test_read_credential_with_fake_data function to test reading a
    cre
    dential with fake data
    - Add create_test_credential fixture to create a test credential
    - Rename test_read_credential function to
    test_read_credential_with_sing
    le_entry

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database import Base
 
-
 @pytest.fixture(scope="function")
 def db_session():
-    # Create an in-memory SQLite database for testing
-    engine = create_engine("sqlite:///:memory:")
+    # Create a file-based SQLite database for testing
+    engine = create_engine("sqlite:///./test.db")
     Base.metadata.create_all(engine)
     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db = TestingSessionLocal()
