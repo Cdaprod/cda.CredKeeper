2fd999dcee223cce3cd5134b7952c66926dd5f39
David <104325852+Cdaprod@users.noreply.github.com>
2023-11-11 21:18:12 -0500

    Refactor test_models.py for improved readability and maintainability.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,18 +1,19 @@
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, CredentialDB
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-TEST_DATABASE_URL = "sqlite:///./test.db"
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, CredentialDB, CredentialType
+
+TEST_DATABASE_URL = "sqlite:///./test.db"  # SQLite will create this file if it doesn't exist
 engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def db_session():
-    models.Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
     try:
         yield db
@@ -20,7 +21,7 @@ def db_session():
         db.close()
 
 def test_create_credential(db_session):
-    new_credential = models.CredentialDB(name="test_credential", value="test_value", type="API_KEY")
+    new_credential = CredentialDB(name="test_credential", value="test_value", type=CredentialType.API_KEY)
     db_session.add(new_credential)
     db_session.commit()
-    assert db_session.query(models.CredentialDB).filter_by(name="test_credential").first() is not None
+    assert db_session.query(CredentialDB).filter_by(name="test_credential").first() is not None
