c5766fa1add3e23913eb4c1de0fef2297459ddcf
David <104325852+Cdaprod@users.noreply.github.com>
2023-11-11 22:28:42 -0500

    Add conftest.py and test_api.py to tests directory

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+@pytest.fixture(scope="function")
+def db_session():
+    # Create an in-memory SQLite database for testing
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(engine)
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,22 +2,21 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi.testclient import TestClient
+import pytest
 from main import app
+from models import CredentialDB, CredentialType
 
 client = TestClient(app)
 
-def test_create_credential():
-    payload = {
-        "name": "test",
-        "value": "secret",  # Make sure this is a valid secret string
-        "type": "API_KEY",  # Ensure this matches the enum in your schema
-        # Include any other required fields defined in your schema
-    }
-    response = client.post("/credentials/", json=payload)
-    assert response.status_code == 200
+@pytest.fixture(scope="function")
+def create_test_credential(db_session):
+    credential = CredentialDB(name="test", value="test_value", type=CredentialType.API_KEY)
+    db_session.add(credential)
+    db_session.commit()
+    yield
+    db_session.delete(credential)
+    db_session.commit()
 
-
-def test_read_credential():
+def test_read_credential(db_session, create_test_credential):
     response = client.get("/credentials/test")
     assert response.status_code == 200
-    assert response.json()["name"] == "test"
